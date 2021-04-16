a201df6402459b6ebd5801b012195509c19530ef
andresvallejoz1991 <andresvallejoz@hotmail.com>
2021-04-16 12:58:52 -0500

    funcion

--- run.py
+++ run.py
@@ -9,12 +9,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return "<h1>Hola mundo con Flask</h1>"
+    return "<h1>Introducción a Flask</h1><h2>@andresvallejoz1991</h2>"
 
 @app.route('/suma')
 def suma():
     resultado = 10 + 10
-    return "<h3>10 + 10 = %d</h3>" % (resultado)
+    return "<h2>El resultado de la Suma es:</h2><h3>10 + 10 = %d</h3>" % (resultado)
+
+@app.route('/listado')
+def listado():
+	return "<h2>Provincias</h2><ul><li>Quito</li><li>Loja</li><li>Azuay</li><li>Ambato</li><li>Riobamba</li><li>Cotopaxi</li><li>Latacunga</li></ul>"
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=True)
